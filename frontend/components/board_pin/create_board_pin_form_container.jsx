7e99610d3bb16c4304054d5e010301e4d5239331
Stephen <stephen.lee.abc@gmail.com>
2019-12-27 12:06:15 -0800

    Create skeleton for creating board_pin

---
+++ frontend/components/board_pin/create_board_pin_form_container.jsx
@@ -0,0 +1,22 @@
+import { connect } from "react-redux";
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+import { createPin } from "../../../actions/pin_actions";
+import CreatePinForm from "./create_board_pin_form";
+
+const mapStateToProps = state => ({
+  currentUser: state.entities.users[state.session.id],
+  pin: { "title": "", "description": "", url: "", photo: null },
+  errors: state.errors.pin,
+  formType: "Create Pin",
+});
+
+const mapDispatchToProps = dispatch => ({
+  processForm: pin => dispatch(createPin(pin)),
+  createBoardPin: boardPin => dispatch(createBoardPin(boardPin)),
+  openModal: modal => dispatch(openModal(modal))
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(CreatePinForm));
+
